73c78582dbe198ad0e4debfbe856c27ff03e3e29
Manish Kumar <67259905+manish-kumar830@users.noreply.github.com>
2023-12-14 06:38:02 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,46 @@
 import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
 
-cv = pickle.load(open("models/cv.pkl", 'rb'))
-clf = pickle.load(open("models/clf.pkl", 'rb'))
+ps = PorterStemmer()
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+tfidf = pickle.load(open('models/vectorizer.pkl','rb'))
+model = pickle.load(open('models/model.pkl','rb'))
 
 def model_predict(email):
     if email == "":
         return ""
-    tokenized_email = cv.transform([email]) # X 
-    prediction = clf.predict(tokenized_email)
-    # If the email is spam prediction should be 1
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. predict
+    prediction = model.predict(vector_input)[0]
     prediction = 1 if prediction == 1 else -1
     return prediction
